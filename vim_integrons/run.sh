7bde9582a9c670a85811806d76edb596881035b7
Finlay Maguire <finlaymaguire@gmail.com>
2023-10-22 13:58:12 -0300

    Ensure contig name in annotation

--- vim_integrons/run.sh
+++ vim_integrons/run.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 for plasmid in ../vim_plasmids/*.fna;
 do
     sample=$(basename $plasmid | cut -d '.' -f1)
-    integron_finder --circ --local-max --cpu 3 --promoter-attI --outdir . --path-func-annot ../../HMM --gbk --pdf $plasmid
+    echo "integron_finder --circ --local-max --cpu 6 --promoter-attI --outdir . --path-func-annot ../../HMM --gbk --pdf $plasmid"
     # AMRfinderPlus HMMs
 done
