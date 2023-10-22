d5f1497523ea23b7106b2116afe24a8306908847
Finlay Maguire <finlaymaguire@gmail.com>
2023-10-22 15:55:10 -0300

    Update plasmid mob

--- vim_plasmids/annotate.sh
+++ vim_plasmids/annotate.sh
@@ -7,13 +7,13 @@
 set -euo pipefail
 
 mkdir -p mob_suite bakta 
-#cat *.fna | prodigal -t bakta/prodigal_training_file -p single # create common prodigal training file from all 3 plasmids for consistency
+cat *.fna | prodigal -t bakta/prodigal_training_file -p single # create common prodigal training file from all 3 plasmids for consistency
 for assembly in *.fna;
 do
     sample=$(echo $assembly | cut -d '.' -f1)
-    #mob_recon -i $assembly -o mob_suite/${sample}
+    mob_recon -i $assembly -o mob_suite/${sample} --force
 done
 
-#bakta --skip-plot --genus "Enterobacter" --species "soli" --strain "patient B" --plasmid "p.AC919" --force --keep-contig-headers --compliant --db ../../../bakta_db/db --output bakta/enterobacter_unicycler_vim_plasmid_aa919 --complete --threads 12 --prodigal-tf bakta/prodigal_training_file enterobacter_unicycler_vim_plasmid_aa919.fna  
+bakta --skip-plot --genus "Enterobacter" --species "soli" --strain "patient B" --plasmid "p.AC919" --force --keep-contig-headers --compliant --db ../../../bakta_db/db --output bakta/enterobacter_unicycler_vim_plasmid_aa919 --complete --threads 12 --prodigal-tf bakta/prodigal_training_file enterobacter_unicycler_vim_plasmid_aa919.fna  
 bakta --skip-plot --genus "Pseudomonas" --species "putida" --strain "patient B" --plasmid "p.AC907" --force --keep-contig-headers --compliant --db ../../../bakta_db/db --output bakta/pputida_trycycler_vim_plasmid_ac907 --complete --threads 12 --prodigal-tf bakta/prodigal_training_file pputida_trycycler_vim_plasmid_ac907.fna
 bakta --skip-plot --genus "Proteus" --species "mirabilis" --strain "patient A" --plasmid "p.AC082" --force --keep-contig-headers --compliant --db ../../../bakta_db/db --output bakta/proteus_unicycler_vim_plasmid_ac082 --complete --threads 12 --prodigal-tf bakta/prodigal_training_file proteus_unicycler_vim_plasmid_ac082.fna
