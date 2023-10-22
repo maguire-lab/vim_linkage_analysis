f47b5fa17908d42eedb71aceb30b897236fd8dc4
Finlay Maguire <finlaymaguire@gmail.com>
2023-10-22 15:00:39 -0300

    Add clinker integron plot and modifications

---
+++ plots/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# using VIM integron locations from integron_finder
+clinker ../vim_plasmids/bakta/*/*.gbff -p clinker_full_gbks.html --ranges enterobacter_vim_aa919:52156-57165 pputida_vim_ac907:147217-151320 proteus_vim_ac082:21986-24508
